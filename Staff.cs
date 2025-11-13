cab0df8375ec16278cb0ed4a2079e21e75349607
denizerdem1 <s31409@pjwstk.edu.pl>
2025-11-13 18:27:11 +0100

    Update Staff.cs
    Added extent persistency methods to Staff class

--- Staff.cs
+++ Staff.cs
@@ -1,55 +1,83 @@
-﻿namespace BYT_Project;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 
-public abstract class Staff
+namespace BYT_Project
 {
-    private string _name;
-    private DateTime _employmentDate;
-    private double _baseSalary;
+    
 
-    public static double YearlySalaryGrowthPercentage = 0.05;
+    public abstract class Staff
+    {
+        private string _name;
+        private DateTime _employmentDate;
+        private double _baseSalary;
+
+        public static double YearlySalaryGrowthPercentage = 0.05;
+    
+        public static List<Staff> Extent = new List<Staff>();
     
     
 
-    public string Name
-    {
-        get => _name;
-        set
+        public string Name
         {
-            if (String.IsNullOrEmpty(value))
+            get => _name;
+            set
             {
-                throw new ArgumentNullException("Name can't be empty");
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Name can't be empty");
+                }
+                _name = value;
             }
-            _name = value;
         }
-    }
 
-    public DateTime EmploymentDate
-    {
-        get => _employmentDate;
-        set
+        public DateTime EmploymentDate
         {
-            if (value == DateTime.MinValue)
+            get => _employmentDate;
+            set
             {
-                throw new ArgumentNullException("Employment Date can't be empty");
+                if (value == DateTime.MinValue)
+                {
+                    throw new ArgumentNullException("Employment Date can't be empty");
+                }
+                _employmentDate = value;
             }
-            _employmentDate = value;
         }
-    }
 
-    public double Salary
-    {
-        get
+        public double Salary
         {
-            int yearsSinceEmployment = EmploymentDate.Year - _employmentDate.Year;
-            return _baseSalary * (1 + YearlySalaryGrowthPercentage * yearsSinceEmployment);
-        }
+            get
+            {
+                int yearsSinceEmployment = EmploymentDate.Year - _employmentDate.Year;
+                return _baseSalary * (1 + YearlySalaryGrowthPercentage * yearsSinceEmployment);
+            }
         
-    }
+        }
 
-    public Staff(string name, DateTime employmentDate, double baseSalary)
-    {
-        _name = name;
-        _employmentDate = employmentDate;
-        _baseSalary = baseSalary;
+        public Staff(string name, DateTime employmentDate, double baseSalary)
+        {
+            _name = name;
+            _employmentDate = employmentDate;
+            _baseSalary = baseSalary;
+        
+            Extent.Add(this);
+        }
+        public static void SaveExtent(string fileName = "staff_extent.json")
+        {
+            var json = JsonSerializer.Serialize(
+                Extent,
+                new JsonSerializerOptions { WriteIndented = true }
+            );
+
+            File.WriteAllText(fileName, json);
+        }
+        public static void LoadExtent(string fileName = "staff_extent.json") {
+            if (!File.Exists(fileName))
+                return;
+
+            var json = File.ReadAllText(fileName);
+            Extent = JsonSerializer.Deserialize<List<Staff>>(json);
+        }
     }
 }
