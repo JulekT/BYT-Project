bcbc35489403dbf9d2eecd24cbf563465d1feda0
s31513 <s31513@pjwstk.edu.pl>
2025-12-05 12:54:09 +0100

    Update ProductTests.cs

--- Tests/ProductTests.cs
+++ Tests/ProductTests.cs
@@ -1,5 +1,6 @@
 using Library;
 using System;
+using System.IO;
 using NUnit.Framework;
 
 namespace Tests
@@ -12,11 +13,14 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
+            Product.Extent.Clear();
+
             emptyProduct = new Product();
             fullProduct = new Product("Laptop", "Lenovo", "IdeaPad 5", 3200, 2500);
         }
 
-        
+
+
         [Test]
         public void ProductNameAssignEmptinessException()
         {
@@ -24,15 +28,47 @@ namespace Tests
             Assert.That(ex.Message, Is.EqualTo("Product name cannot be empty"));
         }
 
-    
         [Test] 
         public void ProductNameGetEmptinessException()
         {
             var ex = Assert.Throws<ValueNotAssigned>(() => Console.WriteLine(emptyProduct.Name));
-            Assert.That(ex.Message, Is.EqualTo("Product name is empty, you need to assign it "));
+            Assert.That(ex.Message, Is.EqualTo("Product name is empty, you need to assign it first"));
+        }
+
+      
+
+        [Test]
+        public void ProductBrandAssignEmptinessException()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => emptyProduct.Brand = "");
+            Assert.That(ex.Message, Is.EqualTo("Brand cannot be empty"));
+        }
+
+        [Test]
+        public void ProductBrandGetEmptinessException()
+        {
+            var ex = Assert.Throws<ValueNotAssigned>(() => Console.WriteLine(emptyProduct.Brand));
+            Assert.That(ex.Message, Is.EqualTo("Product brand is empty, you need to assign it first"));
         }
 
     
+
+        [Test]
+        public void ProductModelAssignEmptinessException()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => emptyProduct.Model = "");
+            Assert.That(ex.Message, Is.EqualTo("Model cannot be empty"));
+        }
+
+        [Test]
+        public void ProductModelGetEmptinessException()
+        {
+            var ex = Assert.Throws<ValueNotAssigned>(() => Console.WriteLine(emptyProduct.Model));
+            Assert.That(ex.Message, Is.EqualTo("Product model is empty, you need to assign it first"));
+        }
+
+    
+
         [Test]
         public void ProductPriceAssignNegativeException()
         {
@@ -40,7 +76,30 @@ namespace Tests
             Assert.That(ex.Message, Is.EqualTo("Price must be positivea"));
         }
 
-        
+        [Test]
+        public void ProductPriceGetNotAssignedException()
+        {
+            var ex = Assert.Throws<NumberIsNotPositive>(() => Console.WriteLine(emptyProduct.Price));
+            Assert.That(ex.Message, Is.EqualTo("Price must be positive, you need to assign it first"));
+        }
+
+     
+
+        [Test]
+        public void ProductCostAssignNegativeException()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => emptyProduct.Cost = -30);
+            Assert.That(ex.Message, Is.EqualTo("Cost must be positive"));
+        }
+
+        [Test]
+        public void ProductCostGetNotAssignedException()
+        {
+            var ex = Assert.Throws<NumberIsNotPositive>(() => Console.WriteLine(emptyProduct.Cost));
+            Assert.That(ex.Message, Is.EqualTo("Product cost must be positive, you need to assign it first"));
+        }
+
+    
         [Test]
         public void ProductExtent_AddsCorrectly()
         {
@@ -52,10 +111,39 @@ namespace Tests
         }
 
         [Test]
+        public void AddNullProductToExtentException()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => Product.AddProductToExtent(null));
+            Assert.That(ex.Message, Is.EqualTo("Product cannot be null"));
+        }
+
+
+        [Test]
+        public void SaveProductExtentFileCreation()
+        {
+            Product.SaveExtent();
+            Assert.That(File.Exists("product_extent.json"), Is.True, "File wasn’t created properly");
+        }
+
+        [Test]
+        public void LoadProductExtentFromFile()
+        {
+            var p = new Product("TV", "Sony", "Bravia", 3000, 2200);
+            Product.AddProductToExtent(p);
+
+            Product.SaveExtent();
+            Product.Extent.Clear();
+
+            Product.LoadExtent();
+
+            Assert.That(Product.Extent.Count > 0, Is.True, "Loading didn’t work");
+        }
+
+
+        [Test]
         public void ProductAisleReverseConnectionWorks()
         {
             var aisle = new Aisle("Laptops");
-
             var product = new Product("Gaming Laptop", "ASUS", "ROG Strix G17", 7500, 6000);
 
             aisle.AddProduct(product);
